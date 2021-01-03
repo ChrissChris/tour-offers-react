a13559641e4e3b8aabad528474bfdb0f34498e99
Chriss Chris <zenskech@outlook.com>
2021-01-03 20:45:13 +0500

    edit

--- tour-offers-react/src/App.js
+++ tour-offers-react/src/App.js
@@ -4,7 +4,32 @@ import Tours from "./Tours";
 
 const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>;
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    const response = await fetch(url);
+    const tour = await response.json();
+    console.log(tour);
+  };
+
+  useEffect(() => {
+    fetchTours();
+  });
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
 export default App;
