8e09b503c8dca28243e4b0793cbb0d68d168fdea
Chriss Chris <zenskech@outlook.com>
2021-01-04 14:29:10 +0500

    edit

--- tour-offers-react/src/Tour.js
+++ tour-offers-react/src/Tour.js
@@ -3,7 +3,7 @@ import React from "react";
 const Tour = ({ id, image, info, price, name }) => {
   return (
     <article className="single-tour">
-      <img src="image" alt="name" />
+      <img src={image} alt="name" />
       <footer>
         <div className="tour-info">{name}</div>
       </footer>
